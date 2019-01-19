ad4c7c403f366199ea5a87fea96a17ee01a41048
Vitthal Jadhav <jadhav.vitthal25@gmail.com>
2019-01-19 15:29:37 +0530

    Dependency Injection

---
+++ Di-demo/src/main/java/guru/springframework/Didemo/DiDemoApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.Didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+}
+
